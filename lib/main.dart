4a02c1fa6989e596f19ccb74dc1da75a5871c1be
FlutLab.io <>
2022-03-03 03:31:26 +0000

    Snapshot 'uts'

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'SignUp.dart';
 
-import 'app/routes/app_pages.dart';
-import 'app/widgets/splash.dart';
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: FriendlychatApp()));
 
-void main() {
-  runApp(MyApp());
-}
+class FriendlychatApp extends StatelessWidget {
+  const FriendlychatApp({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 3)),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return SplashScreen();
-        } else {
-          return GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: "Application",
-            initialRoute: Routes.HOME,
-            getPages: AppPages.routes,
-          );
-        }
-      },
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(40, 90, 40, 60),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: [
+                  Positioned(
+                    child: CircleAvatar(
+                      radius: 55,
+                      backgroundImage: AssetImage('aset/images/small.jpeg'),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Welcome to voluntApp",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "selamat datang di aplikasi ini semoga betah kajdkajdkajdkajdkjakdjakdjakfjakjakj",
+                        overflow: TextOverflow.visible,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 50, 0, 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: SizedBox.fromSize(
+                  size: Size.square(400),
+                  child: Image.asset(
+                    'aset/images/space.jpeg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 400,
+              height: 50,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13.0),
+                  )),
+                ),
+                onPressed: () {
+                  Route route = MaterialPageRoute(builder: (context) => SignUp());
+                  Navigator.push(context, route);
+                },
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
